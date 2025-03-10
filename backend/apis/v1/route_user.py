305ab82b940f3ebf03cafde2fcfe50474a1ed3da
Nathaniel Ince <52634828+uforanch@users.noreply.github.com>
2025-03-10 19:14:34 -0400

    test passes

--- backend/apis/v1/route_user.py
+++ backend/apis/v1/route_user.py
@@ -9,7 +9,7 @@ from db.repository.user import create_new_user
 router = APIRouter()
 
 
-@router.post("/", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
+@router.post("/users", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user, db=db)
     return user
